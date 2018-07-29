40f00a10dd5021552b0bd2430dbef19020059704
disconnect3d <dominik.b.czarnota@gmail.com>
2018-07-29 22:18:34 +0200

    Put info about merging steps to tag_release

--- tag_release.sh
+++ tag_release.sh
@@ -2,6 +2,24 @@
 
 set -ex
 
+# Before making a release, all dev should be 'above beta/stable'
+# so those steps must be done:
+# NOTE: The merges might require `-X theirs`
+#
+# git checkout beta
+# git reset --hard origin/beta
+# git merge stable
+# 
+#
+# git checkout dev
+# git reset --hard origin/dev
+# git merge beta
+#
+## Push the branches:
+#
+# git push beta
+# git push dev
+
 DATE=$(which gdate || which date)
 
 git fetch --all
